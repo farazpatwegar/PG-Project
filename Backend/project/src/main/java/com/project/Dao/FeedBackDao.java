2e529afdc8efa80a2dcc978b8dab4bb60a63a83f
Utkarsha <utkarshachinde09@gmail.com>
2024-08-19 00:19:03 +0530

    remove

--- Backend/project/src/main/java/com/project/Dao/FeedBackDao.java
+++
@@ -1,9 +0,0 @@
-package com.project.Dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.entities.SiteFeedback;
-
-public interface FeedBackDao extends JpaRepository<SiteFeedback, Integer>{
-
-}
